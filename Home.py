62f730ca384c5e095345633bc5e1fc96949246da
jaonineX <suthinancheewapanich@gmail.com>
2024-03-26 15:51:37 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,28 +8,48 @@ import pandas as pd
 
 gender_data = pd.read_csv('./data/onlinefoods.csv')
 
+# Gender Distribution by Age
+html_count_by_age = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Gender Distribution by Age</h3></center>
+</div>
+"""
 st.markdown(html_count_by_age, unsafe_allow_html=True)
 
 plt.figure(figsize=(15, 5))
-sns.countplot(x='Age', hue='Gender', data=gender_data)  # ใช้ countplot โดยกำหนด x เป็น 'Age' และ hue เป็น 'Gender'
+sns.countplot(x='Age', hue='Gender', data=gender_data)
 plt.title('Gender Distribution by Age')
 plt.xlabel('Age')
 plt.ylabel('Count')
 plt.xticks(rotation=45)
 st.pyplot()
 
+# Gender Distribution
+html_gender_distribution = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Gender Distribution</h3></center>
+</div>
+"""
+st.markdown(html_gender_distribution, unsafe_allow_html=True)
+
 plt.figure(figsize=(15, 5))
-sns.countplot(x='Gender', data=gender_data)  # แก้เป็น gender_data แทน shopping_trends.csv
-plt.title('จำนวนเพศตามช่วงอายุ')
-plt.xlabel('เพศ')
-plt.ylabel('จำนวน')
+sns.countplot(x='Gender', data=gender_data)
+plt.title('Gender Distribution')
+plt.xlabel('Gender')
+plt.ylabel('Count')
 plt.xticks(rotation=45)
 st.pyplot()
 
+# Sales by Gender
+html_sales_by_gender = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Sales by Gender</h3></center>
+</div>
+"""
 st.markdown(html_sales_by_gender, unsafe_allow_html=True)
 
 plt.figure(figsize=(15, 5))
-sns.countplot(x='Gender', hue='Output', data=gender_data)  # ใช้ countplot โดยกำหนด x เป็น 'Gender' และ hue เป็น 'Output'
+sns.countplot(x='Gender', hue='Output', data=gender_data)
 plt.title('Sales by Gender')
 plt.xlabel('Gender')
 plt.ylabel('Count')
