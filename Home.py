c69b12bde5cdde2cbb6d1134a733fd834ca86b7f
jaonineX <suthinancheewapanich@gmail.com>
2024-03-26 15:54:46 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,21 +8,17 @@ import pandas as pd
 
 gender_data = pd.read_csv('./data/onlinefoods.csv')
 
-# Gender Distribution by Age
-html_count_by_age = """
-<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
-<center><h3>Gender Distribution by Age</h3></center>
-</div>
-"""
+html_count_by_age = "<center><h3>Gender Distribution by Age</h3></center>"
+
 st.markdown(html_count_by_age, unsafe_allow_html=True)
 
-plt.figure(figsize=(15, 5))
-sns.countplot(x='Age', hue='Gender', data=gender_data)
-plt.title('Gender Distribution by Age')
-plt.xlabel('Age')
-plt.ylabel('Count')
-plt.xticks(rotation=45)
-st.pyplot()
+fig, ax = plt.subplots(figsize=(15, 5))
+sns.countplot(x='Age', hue='Gender', data=gender_data, ax=ax)
+ax.set_title('Gender Distribution by Age')
+ax.set_xlabel('Age')
+ax.set_ylabel('Count')
+ax.tick_params(axis='x', rotation=45)
+st.pyplot(fig)
 
 # Gender Distribution
 html_gender_distribution = """
@@ -32,13 +28,13 @@ html_gender_distribution = """
 """
 st.markdown(html_gender_distribution, unsafe_allow_html=True)
 
-plt.figure(figsize=(15, 5))
-sns.countplot(x='Gender', data=gender_data)
-plt.title('Gender Distribution')
-plt.xlabel('Gender')
-plt.ylabel('Count')
-plt.xticks(rotation=45)
-st.pyplot()
+fig, ax = plt.subplots(figsize=(15, 5))
+sns.countplot(x='Gender', data=gender_data, ax=ax)
+ax.set_title('Gender Distribution')
+ax.set_xlabel('Gender')
+ax.set_ylabel('Count')
+ax.tick_params(axis='x', rotation=45)
+st.pyplot(fig)
 
 # Sales by Gender
 html_sales_by_gender = """
@@ -48,14 +44,10 @@ html_sales_by_gender = """
 """
 st.markdown(html_sales_by_gender, unsafe_allow_html=True)
 
-plt.figure(figsize=(15, 5))
-sns.countplot(x='Gender', hue='Output', data=gender_data)
-plt.title('Sales by Gender')
-plt.xlabel('Gender')
-plt.ylabel('Count')
-plt.xticks(rotation=45)
-st.pyplot()
-
-
-
-
+fig, ax = plt.subplots(figsize=(15, 5))
+sns.countplot(x='Gender', hue='Output', data=gender_data, ax=ax)
+ax.set_title('Sales by Gender')
+ax.set_xlabel('Gender')
+ax.set_ylabel('Count')
+ax.tick_params(axis='x', rotation=45)
+st.pyplot(fig)
